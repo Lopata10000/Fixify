289d9dc9a0f0207f2ed3a29d8ce58a1cda2d719f
Фанта Петро <118938140+Lopata10000@users.noreply.github.com>
2024-02-19 23:02:04 +0100

    First

---
+++ src/test/java/com/fanta/fixify/FixifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanta.fixify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
